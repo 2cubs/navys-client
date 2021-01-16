d16dd36d9551d8e72f35779d2cf304c7284b21a5
Ivan Ponomarev <ivan.ponomarev.pi@gmail.com>
2021-01-16 23:41:53 +0100

    Just a minimal dummy

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from app import App
+# from app import App
+# if __name__ == '__main__':
+#     app = App()
+#     app.run()
+from time import sleep
+
+from navys.client.Client import Client
+
+
+def my_event_cb(service, status):
+    print(f'Event of my event cb!!! service: {service}; status: {status}')
+
 
 if __name__ == '__main__':
-    app = App()
-    app.run()
+    app = Client.get_instance()
+    app.subscribe_to_event(Client.EVENT_SERVICE_STATUS_CHANGED, my_event_cb)
+    remote = app.remote
+
+    server_time = remote.server_time()
+    print(f'Server Time: {server_time}')
+    print(f'Sleeping 20 sec before exiting')
+    sleep(20)
---
+++ navys/client/Client.py
@@ -0,0 +1,13 @@
+from lib2cubs.applevelcom.basic import ClientBase, Remote, action
+
+
+class Client(ClientBase):
+
+	EVENT_SERVICE_STATUS_CHANGED: str = 'service_status_changed'
+
+	def app(self, remote: Remote):
+		pass
+
+	@action
+	def event_service_status_changed(self, service: str, status: str):
+		self.trigger_event(self.EVENT_SERVICE_STATUS_CHANGED, service=service, status=status)
